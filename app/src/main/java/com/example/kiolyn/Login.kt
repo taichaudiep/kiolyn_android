31e746331ab14d649d0f75abfdda9d328ca7949d
Tai Chau <tai.chau@idsolutions.com.vn>
2020-07-13 17:58:43 +0700

    Show popup and otp inpit

--- app/src/main/java/com/example/kiolyn/Login.kt
+++ app/src/main/java/com/example/kiolyn/Login.kt
@@ -1,17 +1,29 @@
 package com.example.kiolyn
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
 import android.view.WindowManager
 import android.widget.Button
-import androidx.core.view.isVisible
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.mukesh.OtpView
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
     lateinit var connect_btn: Button
     lateinit var disconnect_btn: Button
     lateinit var signIn_btn: Button
+
+    lateinit var popupWindow: PopupWindow
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,9 +40,31 @@ class Login : AppCompatActivity() {
         disconnect_btn = findViewById(R.id.disconnect_btn)
     }
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     fun connectClick(view: View) {
         connect_btn.visibility = View.INVISIBLE
         signIn_btn.visibility = View.VISIBLE
         disconnect_btn.visibility = View.VISIBLE
+
+        val layoutInflater = baseContext
+            .getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        val popupView = layoutInflater.inflate(R.layout.popup_login, null)
+
+        popupWindow = PopupWindow(
+            popupView,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        TransitionManager.beginDelayedTransition(root_layout)
+        popupWindow.showAtLocation(root_layout, Gravity.CENTER, 0, 0)
+        popupWindow.isFocusable = true;
+        popupWindow.update();
+    }
+
+    fun acceptClick(view: View) {
+        Toast.makeText(applicationContext, "Connected", Toast.LENGTH_SHORT).show()
+        popupWindow.dismiss()
     }
 }
