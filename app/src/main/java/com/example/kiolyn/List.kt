798be4b09a1b2d64e1cf29baf158605156d8b042
Tai Chau <tai.chau@idsolutions.com.vn>
2020-07-17 17:55:42 +0700

    update show icon in item and cal api

--- app/src/main/java/com/example/kiolyn/List.kt
+++ app/src/main/java/com/example/kiolyn/List.kt
@@ -11,11 +11,12 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 
 class List : AppCompatActivity() {
 
+    lateinit var queue: RequestQueue
     private val GET_URL = "https://postman-echo.com/basic-auth"
 
     lateinit var items: ArrayList<Items>
@@ -39,34 +40,40 @@ class List : AppCompatActivity() {
         rvItems.adapter = adapter
         rvItems.layoutManager = LinearLayoutManager(this)
 
+        queue = Volley.newRequestQueue(this)
+
         swipeLayout.setOnRefreshListener {
             swipeLayout.isRefreshing = !swipeLayout.isRefreshing
-            val queue: RequestQueue = Volley.newRequestQueue(this)
 
-            val stringRequests =
-                StringRequest(Request.Method.GET,
-                    GET_URL,
-                    Response.Listener<String> { response ->
-                        Toast.makeText(
-                            this,
-                            "" + response,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        swipeLayout.isRefreshing = false
-                    },
-                    Response.ErrorListener { response ->
-                        Toast.makeText(
-                            this,
-                            "" + response,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        swipeLayout.isRefreshing = false
-                    })
-            queue.add(stringRequests)
+            val jsonRequest = object : JsonObjectRequest(Request.Method.GET, GET_URL, null,
+                Response.Listener { response ->
+                    Toast.makeText(
+                        this,
+                        "" + response,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    swipeLayout.isRefreshing = false
+                },
+                Response.ErrorListener { response ->
+                    Toast.makeText(
+                        this,
+                        "Json: " + response,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    swipeLayout.isRefreshing = false
+                }) {
+                override fun getHeaders(): MutableMap<String, String> {
+                    val params = HashMap<String, String>()
+                    params["Authorization"] = "Basic cG9zdG1hbjpwYXNzd29yZA=="
+                    return params
+                }
+            }
+
+            queue.add(jsonRequest)
         }
     }
 
-    private fun initializeUi(){
+    private fun initializeUi() {
         rvItems = findViewById<View>(R.id.rv_items) as RecyclerView
         swipeLayout = findViewById(R.id.swipe_refresh)
     }
